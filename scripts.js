a432e67985700f629b34e1317d22d24240a65430
chihebSamti <chiheb.samti@gmail.com>
2022-12-19 00:04:39 +0100

    home page + about page

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById('navLinks');
+
+//  toggle menu
+function showMenu(){
+  navLinks.style.right = "0px";
+}
+
+function hideMenu(){
+  navLinks.style.right = "-400px";
+}
+
