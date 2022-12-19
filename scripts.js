8bc74795bb6e6f34b9b1f3fbd1e7263ca77b5837
chihebSamti <chiheb.samti@gmail.com>
2022-12-20 00:06:45 +0100

    fix navLinks css

--- scripts.js
+++ scripts.js
@@ -2,10 +2,9 @@ var navLinks = document.getElementById('navLinks');
 
 //  toggle menu
 function showMenu(){
-  navLinks.style.right = "0px";
+  navLinks.style.right = "0px"; 
 }
 
 function hideMenu(){
   navLinks.style.right = "-400px";
 }
-
