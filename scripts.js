e2833d2067c7f27c6adcf85438383333f6b028ec
chihebSamti <chiheb.samti@gmail.com>
2022-12-20 12:58:40 +0100

    css bug fix

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-var navLinks = document.getElementById('navLinks');
-
+const navLinks = document.getElementById('navLinks');
+const barsMenu = document.querySelector(".fa-bars");
 //  toggle menu
 function showMenu(){
   navLinks.style.right = "0px"; 
@@ -8,3 +8,24 @@ function showMenu(){
 function hideMenu(){
   navLinks.style.right = "-400px";
 }
+
+function changeColor(element, newColor){
+  element.style.color = newColor;
+}
+
+function vh(percent) {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return (percent * h) / 100;
+}
+
+// change bars menu color
+window.addEventListener('scroll', ()=>{
+  let y = window.scrollY;
+  console.log(y);
+  console.log(vh(50));
+  if (y>= vh(30)) {
+    changeColor(barsMenu, "#000000");
+  }else{
+    changeColor(barsMenu, "#fff");
+  }
+})
